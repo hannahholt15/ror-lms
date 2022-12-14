662dce896b3578547ee6982628a69e65c3709806
hannah holt <hannahholt34@gmail.com>
2022-12-13 18:12:11 -0700

    user done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
+User.delete_all
+
 10.times do
-    Course.create(
-      title: Faker::Educator.course_name,
-      desc: Faker::Quotes::Shakespeare.romeo_and_juliet_quote,
-      ctype: Faker::Educator.subject,
-      course_number: Faker::Number.number(digits: 4)
-    )
-  end
-  
-  puts "# Courses:"
-  puts Course.all.count
+  Course.create(
+    title: Faker::Educator.course_name,
+    desc: Faker::Quotes::Shakespeare.romeo_and_juliet_quote,
+    ctype: Faker::Educator.subject,
+    course_number: Faker::Number.number(digits: 4)
+  )
+
+  User.create(
+    first_name: Faker::FunnyName.name,
+    last_name: Faker::FunnyName.name,
+    email: "#{Faker::Tea.variety}@email.com",
+    password: 'password'
+  )
+end
+
+puts "# Courses:"
+puts Course.all.count
+
+puts "# Users:"
+puts User.all.count
